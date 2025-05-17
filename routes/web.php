bd36139d493bd8d8b106b3f7064562b857c1f8ea
shakilahsan51 <shakilahsan51@gmail.com>
2025-05-17 18:02:50 +0600

    front-end-mastering

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,19 @@ Route::get('/', function () {
     return view('frontend.home');
 });
 
+Route::get('/blog', function () {
+    return view('frontend.blog');
+});
+
+Route::get('/blog-details', function () {
+    return view('frontend.blog-details');
+});
+
+Route::get('/portfolio-details', function () {
+    return view('frontend.portfolio-details');
+});
+
+
 
 Route::get('/dashboard',[DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
