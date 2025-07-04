a28910fe5a9a3c3117186cd867aa4598e7612f85
shakilahsan51 <shakilahsan51@gmail.com>
2025-07-05 00:56:44 +0600

    Footer Section is fully Complete

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -1,58 +1,63 @@
-		<footer class="footer-area">
-			<div class="container">
-				<div class="row footer-widgets">
-					<div class="col-md-12 col-lg-3 widget">
-						<div class="text-box">
-							<figure class="footer-logo">
-								<img src="images/logo.png" alt="">
-							</figure>
-							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, libero. </p>
-							<ul class="d-flex flex-wrap">
-								<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-								<li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
-								<li><a href="#"><i class="fab fa-twitter"></i></a></li>
-								<li><a href="#"><i class="fab fa-behance"></i></a></li>
-							</ul>
-						</div>
-					</div>
-					<div class="col-md-4 col-lg-2 offset-lg-1 widget">
-						<h3 class="widget-title">Useful Link</h3>
-						<ul class="nav-menu">
-							<li><a href="#">Home</a></li>
-							<li><a href="#">About</a></li>
-							<li><a href="#">Portfolio</a></li>
-							<li><a href="#">Blog</a></li>
-						</ul>
-					</div>
-					<div class="col-md-4 col-lg-3 widget">
-						<h3 class="widget-title">Contact Info</h3>
-						<ul>
-							<li>17232 Broadway Suite 308, Jackson Heights, 11372, NY, United States.</li>
-							<li><a href="#">+1347-430-9510</a></li>
-							<li><a href="#">websolutionus1@gmail.com</a></li>
-						</ul>
-					</div>
-					<div class="col-md-4 col-lg-3 widget">
-						<h3 class="widget-title">Help</h3>
-						<ul class="nav-menu">
-							<li><a href="#">Privacy Policy</a></li>
-							<li><a href="#">404 Page</a></li>
-							<li><a href="#">Terms</a></li>
-							<li><a href="#">Documentation</a></li>
-						</ul>
+@php
+	$footerInformation = \App\Models\FooterInfo::first();
+	$footerIcons = \App\Models\FooterSocialLink::all();
+	$footerUsefullLink = \App\Models\FooterUsefullLink::all();
+	$footerContactInfo = \App\Models\FooterContactInfo::first();
+	$FooterHelpLink = \App\Models\FooterHelpLink::all();
+@endphp
+
+<footer class="footer-area">
+	<div class="container">
+		<div class="row footer-widgets">
+			<div class="col-md-12 col-lg-3 widget">
+				<div class="text-box">
+					<figure class="footer-logo">
+						<img src="images/logo.png" alt="">
+					</figure>
+					<p>{{ $footerInformation->info }}</p>
+					<ul class="d-flex flex-wrap">
+						@foreach ($footerIcons as $icons)							
+							<li><a href="{{ $icons->url }}"><i class="{{ $icons->icon }}"></i></a></li>
+						@endforeach
+					</ul>
+				</div>
+			</div>
+			<div class="col-md-4 col-lg-2 offset-lg-1 widget">
+				<h3 class="widget-title">Useful Link</h3>
+				<ul class="nav-menu">
+					@foreach ($footerUsefullLink as $usefullLink)
+						<li><a href="{{ $usefullLink->url }}">{{ $usefullLink->name }}</a></li>
+					@endforeach
+				</ul>
+			</div>
+			<div class="col-md-4 col-lg-3 widget">
+				<h3 class="widget-title">Contact Info</h3>
+				<ul>
+					<li>{{ $footerContactInfo->address }}</li>
+					<li><a href="#">{{ $footerContactInfo->phone }}</a></li>
+					<li><a href="#">{{ $footerContactInfo->email }}</a></li>
+				</ul>
+			</div>
+			<div class="col-md-4 col-lg-3 widget">
+				<h3 class="widget-title">Help</h3>
+				<ul class="nav-menu">
+					@foreach ($FooterHelpLink as $helpLink)
+						<li><a href="{{ $helpLink->url }}">{{ $helpLink->name }}</a></li>
+					@endforeach
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="footer-bottom">
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<div class="copyright">
+						<p>{{ $footerInformation->copy_right }}</p>
+						<p>{{ $footerInformation->powered_by }}</p>
 					</div>
 				</div>
 			</div>
-			<div class="footer-bottom">
-				<div class="container">
-					<div class="row">
-						<div class="col-12">
-							<div class="copyright">
-								<p>Copyright 2023 <span>Rabins</span>. All Rights Reserved.</p>
-								<p>Powered by WebSolutionUS &nbsp; | &nbsp; 2022 - 2023</p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</footer>
+		</div>
+	</div>
+</footer>
