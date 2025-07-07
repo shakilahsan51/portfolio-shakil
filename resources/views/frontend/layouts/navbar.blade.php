ca4dfbecf7d3280968709b7a38ceb54f37f9221b
shakilahsan51 <shakilahsan51@gmail.com>
2025-07-07 11:27:43 +0600

    Project is Done

--- resources/views/frontend/layouts/navbar.blade.php
+++ resources/views/frontend/layouts/navbar.blade.php
@@ -14,24 +14,26 @@
 			<div class="collapse navbar-collapse" id="navbarNav">
 				<ul class="navbar-nav ms-auto">
 					<li class="nav-item">
-						<a class="nav-link active" aria-current="page" href="#home-page">Home</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#about-page">About</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#portfolio-page">Portfolio <i class="fas fa-angle-down"></i></a>
-						<ul class="sub_menu">
-							<li><a href="portfolio.html">Portfolio Grid</a></li>
-						</ul>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#skills-page">Skills</a>
+						<a class="nav-link active" aria-current="page" href="{{ Route::currentRouteName()== 'home' ? 'home-page' : url('/') }}">Home</a>
 					</li>
 
-					<li class="nav-item">
-						<a class="nav-link" href="#contact-page">Contact</a>
-					</li>
+					@if (Route::currentRouteName()== 'home')
+
+						<li class="nav-item">
+							<a class="nav-link" href="#about-page">About</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#portfolio-page">Portfolio</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#skills-page">Skills</a>
+						</li>
+
+						{{-- <li class="nav-item">
+							<a class="nav-link" href="#contact-page">Contact</a>
+						</li> --}}
+
+					@endif
 
 					<li class="nav-item">
 						<a class="nav-link" href="{{  route('blog') }}">Blogs</a>
